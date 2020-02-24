599afc73c280d5c4277051fe974966f98690a4e4
LeoLazc <lazcano.leonel@gmail.com>
2020-02-24 15:04:21 -0300

    Agregado de animaciones en inputs de login y registro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -90,5 +91,6 @@
             @yield('content')
         </main>
     </div>
+    <script type="text/javascript" src="js/input.min.js"></script>
 </body>
 </html>
