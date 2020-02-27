003539f1c3bf909899fe10207b998e28a389ad82
MarCaro <caropompolomaria@gmail.com>
2020-02-27 14:48:34 -0300

    layout para registrados

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,10 @@
-@extends('layouts.app')
+@extends('layouts.appuss')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card todo">
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
